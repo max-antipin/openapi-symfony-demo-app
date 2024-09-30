5bd109294b1c0623cfd84033c3ce1afd608d9679
Maxim Antipin <max.v.antipin@gmail.com>
2024-09-30 13:04:25 +0300

    Add PHP Code Sniffer (phpcs) and fix files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
